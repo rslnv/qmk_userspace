2197421dd90f778ada901c7178106b862d4500cd
Ruslan V <vruslan@gmail.com>
2025-01-03 11:43:05 +0000

    removing transparent mappings

--- keyboards/redox/wireless/keymaps/rslnv_tiny/keymap.c
+++ keyboards/redox/wireless/keymaps/rslnv_tiny/keymap.c
@@ -79,13 +79,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬─────────┬─────────┬─────────┬────────┐                                           ┌────────┬─────────┬────────┬──────────┬────────┬────────┐
      XXXXXXX ,KC_F1   ,KC_F2    ,KC_F3    ,KC_F4    ,KC_F5   ,                                            KC_F6   ,KC_F7    ,KC_F8   ,KC_F9     ,KC_F10  ,XXXXXXX ,
   //├────────┼────────┼─────────┼─────────┼─────────┼────────┼────────┐                         ┌────────┼────────┼─────────┼────────┼──────────┼────────┼────────┤
-     XXXXXXX ,KC_1    ,KC_2     ,KC_3     ,KC_4     ,KC_5    ,_______ ,                          _______ ,KC_6    ,KC_7     ,KC_8    ,KC_9      ,KC_0    ,XXXXXXX ,
+     XXXXXXX ,KC_1    ,KC_2     ,KC_3     ,KC_4     ,KC_5    ,XXXXXXX ,                          XXXXXXX ,KC_6    ,KC_7     ,KC_8    ,KC_9      ,KC_0    ,XXXXXXX ,
   //├────────┼────────┼─────────┼─────────┼─────────┼────────┼────────┤                         ├────────┼────────┼─────────┼────────┼──────────┼────────┼────────┤
      XXXXXXX ,KC_LALT ,CKC_HOME ,CKC_PGUP ,CKC_PGDN ,KC_END  ,KC_F11  ,                          KC_F12  ,KC_LEFT ,CKC_DOWN ,CKC_UP  ,CKC_RIGHT ,KC_RALT ,XXXXXXX ,
   //├────────┼────────┼─────────┼─────────┼─────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼─────────┼────────┼──────────┼────────┼────────┤
-     _______ ,XXXXXXX ,XXXXXXX  ,XXXXXXX  ,XXXXXXX  ,XXXXXXX ,QK_BOOT ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX  ,XXXXXXX ,XXXXXXX   ,XXXXXXX ,_______ ,
+     XXXXXXX ,XXXXXXX ,XXXXXXX  ,XXXXXXX  ,XXXXXXX  ,XXXXXXX ,QK_BOOT ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX  ,XXXXXXX ,XXXXXXX   ,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼─────────┼─────────┼────┬────┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬─────┼────────┼──────────┼────────┼────────┤
-     XXXXXXX ,XXXXXXX ,XXXXXXX  ,XXXXXXX  ,     _______  ,    _______ ,_______ ,        _______ ,_______ ,    _______ ,      XXXXXXX ,XXXXXXX   ,XXXXXXX ,XXXXXXX
+     XXXXXXX ,XXXXXXX ,XXXXXXX  ,XXXXXXX  ,     XXXXXXX  ,    _______ ,_______ ,        _______ ,_______ ,    XXXXXXX ,      XXXXXXX ,XXXXXXX   ,XXXXXXX ,XXXXXXX
   //└────────┴────────┴─────────┴─────────┘    └─────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘     └────────┴──────────┴────────┴────────┘
   ),
 };
